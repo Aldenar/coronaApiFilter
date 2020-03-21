4ce228529d65603836ad6216d1ec59f6439efc94
Aldar <aldar0@centrum.cz>
2020-03-21 15:35:28 +0000

    First Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Apify Corona filter
+
+A quick and dirty solution to a project me and my friends worked on together. 
+
+Our goal was to display current Coronavirus situation data on a tiny ESP we received at one of Avast's CTF days. Unfortunately, the thing has little to no memory, and as we chose to use MicroPython, it was a pain to even initiate a HTTPS connection.
+
+The result was me sitting down and scribbling down a little script that would act as a filter for the (relatively) big dataset returned by the original API, as well as being accessible over plane HTTP.
+
+## Functionality
+1. Returns specific json-encoded data from the backend API
+2. Caches data by default for 10 minutes
+
+## Requirements
+1. The PHP Curl library (Under debian provided by the php-curl package)
+
+## Deployment
+Make the PHP file available under a publically accessible URL and make sure the script can write to /tmp
---
+++ getCoronaInfo.php
@@ -0,0 +1,96 @@
+<?php
+
+    function fetchFromApi(){
+        $curlSession = curl_init();
+        curl_setopt($curlSession, CURLOPT_USERAGENT, 'CoronaTrackerFilterAPI/1.0');
+        curl_setopt($curlSession, CURLOPT_FAILONERROR, true);
+        curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curlSession, CURLOPT_URL, 'https://api.apify.com/v2/key-value-stores/K373S4uCFR9W1K8ei/records/LATEST?disableRedirect=true');
+
+        $jsonData = curl_exec($curlSession);
+        
+        if (curl_errno($curlSession)) {
+            curl_close($curlSession);
+            return 1;
+        }
+        curl_close($curlSession);
+        return $jsonData;
+    }
+
+    function getPayload(){
+        $DEBUG = 0;
+		$cacheStaleTime = 360;
+        $cacheFile = "/tmp/coronaCache";
+        $timestamp = time();
+   
+        if (file_exists($cacheFile) && is_readable($cacheFile))
+        {
+            if ($DEBUG) echo "CACHE READABLE".PHP_EOL;
+
+            $json=json_decode(file_get_contents($cacheFile));
+            if ( ($timestamp - $json->recvAt) > $cacheStaleTime) {
+                if ($DEBUG) echo "CACHE STALE".PHP_EOL;
+                # If fetching new data failed, return stale cache
+                $newJson = fetchFromApi();
+                if ($newJson == 1) {
+                    return $json;
+                }
+                #Else prepare new data, replace stale cache and return it
+                $jsonData = json_decode($newJson);
+                $data = new \stdClass();
+            
+                $data->infected = $jsonData->infected;
+                $data->totalTested = $jsonData->totalTested;
+                $data->recovered = $jsonData->recovered;
+                $data->lastUpdatedAtSource = $jsonData->lastUpdatedAtSource;
+                $data->dead = 0;
+	            foreach ($jsonData->infectedByRegion as $region)
+			    {
+	        		if ($region->name == "Hlavní město Praha")
+	        		{
+	            		$data->infectedPrague = $region->value;
+	        		}
+	    		}
+                $data->recvAt = $timestamp;
+                file_put_contents($cacheFile, json_encode($data));
+                return $data;
+            } else {
+                # Horay, our cache is still current! Simply return it then
+                return $json;
+            }
+        } else { #If we have no cache present yet...
+            $json = fetchFromApi();
+            if ($json == 1)
+            {
+                #If we got an error fetching data here... No way to recover but to return an error...
+                echo '{"error":"REMOTE_API_ERROR"}';
+                exit(1);
+            }
+            #Else we parse the new data, save it to cache and return it
+			$jsonData = json_decode($json);
+            $data = new \stdClass();
+
+            $data->infected = $jsonData->infected;
+            $data->totalTested = $jsonData->totalTested;
+            $data->recovered = $jsonData->recovered;
+            $data->lastUpdatedAtSource = $jsonData->lastUpdatedAtSource;
+            foreach ($jsonData->infectedByRegion as $region)
+		    {
+        		if ($region->name == "Hlavní město Praha")
+        		{
+            		$data->infectedPrague = $region->value;
+        		}
+    		}
+
+			$data->dead = 0;
+            $data->recvAt = $timestamp;
+            file_put_contents($cacheFile, json_encode($data));
+            return $data;
+        }
+    }
+
+    header('Content-Type: application/json');
+
+    $jsonData = getPayload();
+    echo json_encode($jsonData)
+    ?>
